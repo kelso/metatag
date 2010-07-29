83352b42da91e268f50b7456aa768aa24d871eff
Stefan Huska <stefan.huska@gmail.com>
2010-07-29 23:31:43 +0200

    added support for charset meta tag to render_meta_tags method. defaults to utf-8

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,16 +1,18 @@
 module Metatag
   module Helpers
-    
+
     def render_meta_tags(options={})
       @meta_data ||= {}
       site = @meta_data && @meta_data[:site] ? @meta_data[:site] : options[:site]
       full_title = [title_tag, site].compact.join(" | ")
+      charset = options[:charset] || "utf-8"
 
       ret = []
       ret << content_tag(:title, full_title)
       ret << tag(:meta, :name => "description", :content => @meta_data[:description])
       ret << tag(:meta, :name => "keywords", :content => @meta_data[:keywords])
-      return ret.join("\n")
+      ret << tag(:meta, :'http-equiv' => "Content-Type", :content => "text/html; charset=" + charset)
+      ret.join("\n")
     end
 
     def set_meta_tags(*args)
